dc724c679c75e60608fed292b9259135e50c0b19
ruchitnigam <nigam21ruchit@gmail.com>
2025-06-23 19:50:50 +0530

    added unit testcases

--- src/test/java/com/journal/app/JournalApp/JournalAppApplicationTests.java
+++ src/test/java/com/journal/app/JournalApp/JournalAppApplicationTests.java
@@ -1,13 +1,17 @@
-//package com.journal.app.JournalApp;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class JournalAppApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.journal.app.JournalApp;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class JournalAppApplicationTests {
+
+	@InjectMocks
+	private JournalAppApplication journalAppApplication;
+
+	@Test
+	void contextLoads() {
+		assertDoesNotThrow(() -> JournalAppApplication.main(new String[]{}));
+	}
+}
